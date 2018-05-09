913d8df7f017829a7aadf65b3bbbce0c8a98e85b
David Buckley <buckley.w.david@gmail.com>
2018-05-09 13:48:29 -0400

    add a convienence script and remove unused (and quite large) zip file (#1)
    add a convienence script and remove unused (and quite large) zip file
    
    (cherry picked from commit ecf4c00f82b2db138cc2395b3975bb6c8e589d4d)

---
+++ lambda/build_env.sh
@@ -0,0 +1,8 @@
+mkdir -p lambda_functions
+wget -q -O - https://api.github.com/repos/dhs-ncats/lambda_functions/tarball | tar xz --strip-components=1 -C lambda_functions
+cd lambda_functions
+docker-compose build
+docker-compose up
+cp *.zip ../envs
+cd ..
+rm -rf lambda_functions

620e798aa766b6f2806b1e8bd639b1758db16cb9
David Buckley <buckley.w.david@gmail.com>
2018-05-23 13:41:46 -0400

    add missing directory creation (#2)

--- lambda/build_env.sh
+++ lambda/build_env.sh
@@ -1,4 +1,5 @@
 mkdir -p lambda_functions
+mkdir -p env
 wget -q -O - https://api.github.com/repos/dhs-ncats/lambda_functions/tarball | tar xz --strip-components=1 -C lambda_functions
 cd lambda_functions
 docker-compose build
